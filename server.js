7f4b0d96c4e4062de6759a29efc070757ff8a835
optiwari.india <optiwari.india@gmail.com>
2024-05-24 23:48:53 +0000

    changed routes

--- server.js
+++ server.js
@@ -4,20 +4,36 @@ import fileUpload from "express-fileupload";
 import getLocation from "./utils/getLocation.js";
 import cors from "cors";
 
-(()=>{
-    const app=express();
+(() => {
+    const app = express();
     app
         .use(express.json())
-        .use(fileUpload())
+        
         .use(pureIP)
         .use(cors())
-        .use(async (req,res,next)=>{
-            let whoisInfo=await getLocation(req.clientIP);
-            console.log(whoisInfo)
-            let cn=(whoisInfo["country"] instanceof Array)?whoisInfo["country"].find(cn=>cn!="zz"):whoisInfo["country"]
-            res.json({
-                ip:req.clientIP??"",
-                cn
+        
+        app.route("/api/v1/country")
+            .get(async (req, res) => {
+                let whoisInfo = await getLocation(req.clientIP);
+                console.log(whoisInfo)
+                let cn = (whoisInfo["country"] instanceof Array) ? whoisInfo["country"].find(cn => cn != "zz") : whoisInfo["country"]
+                res.json({
+                    ip: req.clientIP ?? "",
+                    cn
+                })
+                // get country code
+            })
+            .post(async (req, res) => {
+                let whoisInfo = await getLocation(req.body.ip??req.clientIP);
+                let cn = (whoisInfo["country"] instanceof Array) ? whoisInfo["country"].find(cn => cn != "zz") : whoisInfo["country"]
+                res.json({
+                    ip: req.body.ip??req.clientIP ?? "",
+                    cn
+                })
+            })
+        app.use(async (req, res, next) => {
+            return res.status(401).json({
+                error: "Unauthorized"
             })
         })
         .listen(3000)
